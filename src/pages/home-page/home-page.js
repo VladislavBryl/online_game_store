812050e27bdca8333f9e2d05089d7d398dee88ae
VladislavBryl <54996000+VladislavBryl@users.noreply.github.com>
2022-02-19 19:29:14 +0200

    update -4

--- src/pages/home-page/home-page.js
+++ src/pages/home-page/home-page.js
@@ -7,7 +7,7 @@ const GAMES = [
         image: '/game-covers/forza_5.jpeg',
         title: 'Forza Horizon 5',
         genres: ['Гонки', 'Симулятор', 'Відкритий світ'],
-        price: 2343,
+        price: 1199,
         video: 'https://www.youtube.com/embed/FYH9n37B7Yw',
         id: 1,
         description: "На вас чекає нескінченний калейдоскоп пригод Horizon! Здійснюйте захоплюючі поїздки неймовірно красивим і самобутнім світом Мексики за кермом найбільших автомобілів в історії. Почніть свою пригоду Horizon вже сьогодні, додавши гру до свого списку бажань!",
@@ -16,7 +16,7 @@ const GAMES = [
         image: '/game-covers/sims_4.jpeg',
         title: 'Sims 4',
         genres: ['Симулятор життя', 'Симулятор навчання'],
-        price: 750,
+        price: 1199,
         video: 'https://www.youtube.com/embed/z00mK3Pxc8w',
         id: 2,
         description: "Дайте волю своїй уяві та створіть світ з унікальними сімами! Вибирайте та змінюйте будь-які деталі як симів, так і їхніх будинків. І це ще не все! Вибирайте зовнішність, характер та вбрання для своїх симів та вирішуйте, як вони будуть проводити свої дні.\n" +
@@ -30,7 +30,7 @@ const GAMES = [
         title: 'Battlefield 2042',
         genres: ['Екшн', 'Шутер', 'Війна'],
         video: 'https://www.youtube.com/embed/ASzOzrB-a9E',
-        price: 2433,
+        price: 1389,
         id: 3,
         description: 'Battlefield™ 2042 – це шутер від першої особи, в якому серія повертається до легендарних масштабних битв. У майбутньому, де панує хаос, адаптуйтеся і процвітайте на полях бою, що постійно змінюються, завдяки своєму загону та арсеналу новітніх технологій.'
     },
@@ -39,7 +39,7 @@ const GAMES = [
         title: 'Life is Strange True Colors',
         genres: ['Глибокий сюжет', 'Протагоністка'],
         video: 'https://www.youtube.com/embed/b6CkzwVAr0M',
-        price: 3021,
+        price: 1477,
         id: 4,
         description: 'Алекс Чень від усіх приховує своє «прокляття» — надприродну здатність зчитувати сильні емоції інших та впливати на них. Але коли її брат гине — нібито внаслідок нещасного випадку, Алекс використовує її, щоб дізнатися правду.'
     },
@@ -48,7 +48,7 @@ const GAMES = [
         title: 'Grand Theft Auto V',
         genres: ['Відкритий світ', 'Екшн'],
         video: 'https://www.youtube.com/embed/QkkoHAzjnUs',
-        price: 789,
+        price: 456.00,
         id: 5,
         description: 'Grand Theft Auto V для PC дозволяє гравцям досліджувати знаменитий світ Лос-Сантоса та округу Блейн у роздільній здатності до 4k і вище з частотою 60 кадрів на секунду.'
     },
@@ -57,7 +57,7 @@ const GAMES = [
         title: 'Tom Clancy\'s Rainbow Six® Siege',
         video: 'https://www.youtube.com/embed/6wlvYh0h63k',
         genres: ['Тактика', 'Шутер'],
-        price: 982,
+        price: 251.00,
         id: 6,
         description: 'Tom Clancy\'s Rainbow Six Осада – це продовження гучного шутера від першої особи, розробленої студією Ubisoft Montreal.'
     },
@@ -66,7 +66,7 @@ const GAMES = [
         title: 'Assassin’s Creed Valhalla',
         genres: ['Паркур', 'РПГ', 'Відкритий світ'],
         video: 'https://www.youtube.com/embed/ssrNcwxALS4',
-        price: 2863,
+        price: 915.00,
         id: 7,
         description: 'Assassin\'s Creed Valhalla – мультиплатформенна комп\'ютерна гра у жанрі action/RPG, розроблена студією Ubisoft Montreal під видавництвом компанії Ubisoft. Є дванадцятою грою у серії ігор Assassin Creed.'
     },
@@ -75,9 +75,33 @@ const GAMES = [
         title: 'Baldur`s gate III',
         genres: ['РПГ', 'Квест', 'Стратегічна гра'],
         video: 'https://www.youtube.com/embed/t9yyRvAiXvc',
-        price: 10000,
+        price: 964.00,
         id: 8,
         description: 'Baldur Gate III – це третина знаменитої рольової серії, яка відійшла від BioWare до Larian. Гра будується на пятій версії правил Dungeons and Dragons, а також розповідає нову історію. У центрі уваги - раптова атака іллітідів, свіжів розуму, які нападають на міста та селища з метою викрадення людей. Головний герой та його потенційні компаньйони – одні з викрадених людей, яким іллітиди вживили в голову своїх личинок. Героям належить знайти спосіб позбутися їх доти, як завершиться цереброморфирование. Третя частина серії заснована на пятій редакції правил D&D, тому багато речей, знайомих фанатам оригінальної дилогії змінилися. Наприклад, бойова система в BG3 - покрокова, а чи не RTWP, одночасно групи може бути трохи більше чотирьох компаньйонів тощо'
+    },
+    {
+        image: '/game-covers/witcher3.jpg',
+        title: 'The Wither 3: Wild Hunt',
+        genres: ['РПГ', 'Відкритий світ', 'Глибокий сюжет', 'Action-adventure'],
+        video: 'https://www.youtube.com/embed/53MyR_Z3i1w',
+        price: 539.00,
+        id: 9,
+    },
+    {
+        image: '/game-covers/detroit.jpg',
+        title: 'Detroit: Become Human',
+        genres: ['Пригодницький бойовик', 'Глибокий сюжет', 'Інтерактивне кіно'],
+        video: 'https://www.youtube.com/embed/QD1pbWCJcKQ',
+        price: 637.00,
+        id: 10,
+    },
+    {
+        image: '/game-covers/FIFA22.jpg',
+        title: 'FIFA 22',
+        genres: ['Футбольний симулятор', 'Спортивна гра'],
+        video: 'https://www.youtube.com/embed/wC-AGjMoRa4',
+        price: 500.00,
+        id: 11,
     },
 ]
 
